e6dc348acf6daaf0160efce50a364555819d6354
Javier E Montes <jaxloz2002@gmail.com>
2024-04-30 23:19:55 -0500

    estilizado de la pagina de registro

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="bg-[#f9fafb] flex flex-col h-screen items-center justify-center font-sans">
         
-        <form class="px-8 py-5 bg-white rounded-md flex flex-col gap-y-4 drop-shadow-lg" submit.prevent="login">
+        <form class="px-8 pt-9 pb-5 bg-white rounded-md flex flex-col gap-y-4 drop-shadow-lg" @submit.prevent="login">
             <div class="text-center">
-                <h1 class="text-3xl mb-5" >Login</h1>
+                <h1 class="text-3xl mb-5 font-semibold" >Login</h1>
             </div>    
             <div class="flex flex-col gap-y-2">
-                <label class="text-base font-medium text-gray-900" for="email">Email</label>
+                <label class="text-base font-medium text-gray-900" for="email">Email address</label>
                 <input class="h-9 w-80 py-2 px-2 rounded-md ring-inset ring-1 ring-gray-300 focus:ring-inset focus:ring-2 focus:ring-[#4f46e5] outline-none" type="email" id="email" v-model="formLogin.email" required>
             </div>
 
@@ -17,9 +17,9 @@
             <div class="flex flex-row">
             <button class="bg-[#4f46e5] hover:bg-[#6366f1] text-white font-medium py-1.5 px-5 w-80 rounded-md" type="submit">Login</button>
             </div>
-            <div class="flex flex-row justify-center gap-x-1" >
-            <spam class="block text-sm text-gray-500" >don't have an account yet?</spam>
-            <router-link to="/registro" class="block font-semibold text-[#4339ff] hover:text-[#6366f1]" >Register</router-link>
+            <div class="flex flex-row justify-center gap-x-1 items-baseline mt-3" >
+            <p class="block text-sm text-gray-500" >don't have an account yet?</p>
+            <router-link to="/registro" class="block font-semibold text-[#4339ff] hover:text-[#6366f1]" >Register now</router-link>
             </div>
         </form>
     </div>
--- src/views/auth/Registro.vue
+++ src/views/auth/Registro.vue
@@ -1,31 +1,62 @@
 <template>
-     <form @submit.prevent="submit">
-    <label for="primerNombre">Primer Nombre:</label>
-    <input v-model="formRegister.firstname" type="text" id="primerNombre" name="primerNombre" required><br><br>
+  <div class="bg-[#f9fafb] h-screen flex flex-col items-center justify-center" >
+    <div class="bg-white px-8 pt-10 pb-5 drop-shadow-lg rounded-md flex flex-col items-center justify-center gap-y-5" >
+      
+      <div class="text-center flex flex-col gap-y-2">
+        <h1 class="block text-3xl font-semibold" >Register</h1>
+        <p class="block text-sm text-gray-500" >get your account</p>
+      </div>
+    
+      <div>  
+      <form @submit.prevent="submit" class="flex flex-col justify-center content-center">
+        <div class="flex flex-col h-20">
+          <label class="text-base font-medium text-gray-900" for="primerNombre">Primer Nombre:</label>
+          <input class="h-9 w-80 py-2 px-2 rounded-md ring-inset ring-1 ring-gray-300 focus:ring-inset focus:ring-2 focus:ring-[#4f46e5] outline-none" v-model="formRegister.firstname" type="text" id="primerNombre" name="primerNombre" required><br><br>
+        </div>
+        
+        <div class="flex flex-col gap-y-2 h-20">
+        <label class="text-base font-medium text-gray-900" for="segundoNombre">Segundo Nombre:</label>
+        <input class="h-9 w-80 py-2 px-2 rounded-md ring-inset ring-1 ring-gray-300 focus:ring-inset focus:ring-2 focus:ring-[#4f46e5] outline-none" v-model="formRegister.lastname" type="text" id="segundoNombre" name="segundoNombre"><br><br>
+        </div>
 
-    <label for="segundoNombre">Segundo Nombre:</label>
-    <input v-model="formRegister.lastname" type="text" id="segundoNombre" name="segundoNombre"><br><br>
+        <div class="flex flex-col gap-y-2 h-20">
+        <label class="text-base font-medium text-gray-900" for="telefono">Teléfono:</label>
+        <input class="h-9 w-80 py-2 px-2 rounded-md ring-inset ring-1 ring-gray-300 focus:ring-inset focus:ring-2 focus:ring-[#4f46e5] outline-none" v-model="formRegister.phone" type="tel" id="telefono" name="telefono" pattern="[0-9]{10}" required><br><br>
+        </div>
+        
+        <div class="flex flex-col gap-y-2 h-20">
+          <label class="text-base font-medium text-gray-900" for="email">Email:</label>
+          <input class="h-9 w-80 py-2 px-2 rounded-md ring-inset ring-1 ring-gray-300 focus:ring-inset focus:ring-2 focus:ring-[#4f46e5] outline-none" v-model="formRegister.email" type="email" id="email" name="email"><br><br>
+        </div>
+        
+        <div class="flex flex-col gap-y-2 h-20">
+          <label class="text-base font-medium text-gray-900" for="password">Password:</label>
+          <input class="h-9 w-80 py-2 px-2 rounded-md ring-inset ring-1 ring-gray-300 focus:ring-inset focus:ring-2 focus:ring-[#4f46e5] outline-none" v-model="formRegister.user_password" type="password" id="password" name="password" required><br><br>
+        </div>
+        
+        <div class="flex flex-col gap-y-2 h-20">
+        <label class="text-base font-medium text-gray-900" for="rol">Rol:</label>
+        
+        <select class=" h-9 w-80 py-1 px-2 rounded-md ring-inset ring-1 ring-gray-300 focus:ring-inset focus:ring-2 focus:ring-[#4f46e5] outline-none" v-model="formRegister.id_rol" id="rol" name="rol" required placeholder="Select rol">
+          <option value="2">Host</option>
+          <option value="3">Publisher</option>
+          <option value="1">Default</option>
+        </select><br><br>
+        </div>
 
-    <label for="telefono">Teléfono:</label>
-    <input v-model="formRegister.phone" type="tel" id="telefono" name="telefono" pattern="[0-9]{10}" required><br><br>
+        <div class="mb-4">
+          <input class="bg-[#4f46e5] hover:bg-[#6366f1] text-white font-medium py-1.5 px-5 w-80 rounded-md" type="submit" value="Register">
+        </div>
 
-    <label for="email">Email:</label>
-    <input v-model="formRegister.email" type="email" id="email" name="email"><br><br>
-
-    <label for="password">Password:</label>
-    <input v-model="formRegister.user_password" type="password" id="password" name="password" required><br><br>
-
-    <label for="rol">Rol:</label>
-
-    <select v-model="formRegister.id_rol" id="rol" name="rol" required>
-      <option value="2">Host</option>
-      <option value="3">Publisher</option>
-      <option value="1">Default</option>
-    </select><br><br>
-
-    <input type="submit" value="Registrarse">
-  </form>
-</template>
+        <div class="flex flex-row justify-center items-center gap-x-1" >
+          <p class="block text-sm text-gray-500" >Already have an account?</p>
+          <router-link to="/login" class="block font-semibold text-[#4339ff] hover:text-[#6366f1]" >Login</router-link>
+        </div>
+      </form>
+  </div>
+  </div>
+  </div>
+  </template>
 <script setup>
 import { useAuthStore } from "@/stores/auth.js"
 import {ref} from "vue"
